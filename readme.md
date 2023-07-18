4dc5e0910bbb0e59388c25235e0080315643b476
Filip Øskar Łanecki <9296944+orhid@users.noreply.github.com>
2023-07-18 05:07:36 -0300

    init

---
+++ readme.md
@@ -0,0 +1,19 @@
+a lua library for basic slurm operations. this was mostly for my limited needs and has room for much more development.
+
+*tested only on the clusters at ACK Cyfronet AGH*
+
+# usage
+both commands `sluarm.sbatch` and `sluarm.srun` take two arguments: the command to be executed as a string and a table of parameters to slurm. both return the jobid of the submitted job.
+
+```lua
+local params = {}
+params.time = '00:36:00'
+params.job-name = 'example-job'
+params.nodes = '3'
+
+local sluarm = require 'sluarm'
+local jobid = sluarm.sbatch('batch-script', params)
+print(jobid)
+```
+
+the command `sluarm.status` takes a jobid and returns its status
---
+++ sluarm.lua
@@ -0,0 +1,35 @@
+local sluarm = {}
+
+os.capture = function (cmd)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
+local stringify_params = function (params)
+  local output = ''
+  for key, value in pairs(params) do
+    output = output .. ' --' .. key .. ' ' .. value
+  end
+  return output
+end
+
+sluarm.srun = function (command, params)
+  local jobid = os.capture('./srun' .. stringify_params(params) .. command):gsub('[^%d]', '')
+  return jobid
+end
+
+sluarm.sbatch = function (command, params)
+  local jobid = os.capture('./sbatch' .. stringify_params(params) .. command):gsub('[^%d]', '')
+  return jobid
+end
+
+sluarm.status = function (jobid)
+  os.capture('sacct -nPX -o State -j ' .. jobid)
+end
+
+return sluarm
